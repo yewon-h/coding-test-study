887301924655fe551b37a848cde6e1d5a4bfdced
kiteha03 <kiteha03@gmail.com>
2026-07-07 16:24:36 +0900

    Create hayeon.py

---
+++ level1/07.05/푸드파이트대회/hayeon.py
@@ -0,0 +1,11 @@
+def solution(food):
+    answer = ''
+    
+    for i in range(1,len(food)):
+        if food[i] // 2 > 0 :
+            answer += str(i) * (food[i] //2 )
+            
+            
+    answer = answer + "0" + answer[::-1]
+    
+    return answer
