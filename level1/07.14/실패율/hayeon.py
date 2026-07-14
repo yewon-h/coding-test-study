a786dc65b41a4c1c04576ed816514bde4773eb0d
kiteha03 <kiteha03@gmail.com>
2026-07-14 19:02:38 +0900

    Create hayeon.py

---
+++ level1/07.14/실패율/hayeon.py
@@ -0,0 +1,35 @@
+def solution(N, stages):
+    answer = []
+    stages = sorted(stages)
+    rem_hum = len(stages)
+    temp_list = []
+    temp_dic = {}
+    
+    for i in range(1,N+1):
+      # 빈 스테이지는 0으로
+        if i not in stages:
+            temp_dic[i] = 0
+        else:
+          # 남은 사람이 없다면 0 으로 설정, 중간에 남은사람이 0이 된다면 아래 나누기에서 에러
+            if rem_hum == 0:
+                temp_dic[i] = 0
+            else:
+              # 스테이지에 존재하는 사람/ 남은사람
+              # key = 실패율  , value = 스테이지
+                count_hum = stages.count(i)
+                temp_dic[i] = count_hum/rem_hum
+                rem_hum -= count_hum
+
+  # 정렬을 위해 딕셔너리를 2차원 배열로 옮겨줌,  이때 -를 붙이는 이유가 실패율로 내림차순을 정렬하고, 같을때 2열은 자동 오름차순으로 정렬하기 위함
+    for k,v in temp_dic.items():
+        temp_list.append([-v,k])
+        
+    temp_list = sorted(temp_list)
+
+  # 스테이지만 따오기
+    for j in range(len(temp_list)):
+        answer.append(temp_list[j][1])
+    
+
+    
+    return answer
