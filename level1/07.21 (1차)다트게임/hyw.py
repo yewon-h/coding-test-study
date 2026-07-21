5e565008aac03266237dcbceb703fd1ac942db00
Yewon Han <hyw@ajou.ac.kr>
2026-07-21 22:04:14 +0900

    주석수정

--- level1/07.21 (1차)다트게임/hyw.py
+++ level1/07.21 (1차)다트게임/hyw.py
@@ -1,12 +1,12 @@
 def solution(dartResult):
     
-    dartResult = dartResult.replace('10', 't')
+    dartResult = dartResult.replace('10', 't') #10점을 하나로 읽어올 수 있게 문자로 치환
     # 마지막에 다 더할거니까 첫 시작을 무조건 0으로 넣어버림 첫 점수 + 스타상받았을 때 하나 앞 *2할 때 인덱스에러 안나게
     scores = []
     score = 0
     
     for i in dartResult:
-        if i == 't': # 첫 점수가 10일때 따로 처리
+        if i == 't': # 점수가 10일때 따로 처리
             scores.append(score) # 첫 점수일 때 0들어가고 이후에는 다음 점수 만날때 이전 [점수 + sdt/*#] 계산결과 넣음
             score = 10 # 앞으로 계산해나갈 점수, score안에서 모두 연산되고 다음 점수 만나면 scores안에 원소로 들어감
             
