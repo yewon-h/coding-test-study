2b599324fde02089de83245151e5a99d2c97799f
Yewon Han <hyw@ajou.ac.kr>
2026-07-22 17:25:27 +0900

    이 라이브러리 쓰면 된대

--- level1/07.22 완주하지 못한 선수/hyw.py
+++ level1/07.22 완주하지 못한 선수/hyw.py
@@ -16,3 +16,17 @@ def solution(participant, completion):
             
 # 안됨 계속 시간복잡도때매 결국 찾아봤는데 이제 for while 로 슬슬 안풀리나봐
 # 딕셔너리나 정렬해서 푸는 방법으로는 풀리는 것 같아
+
+"""
+from collections import Counter
+
+def solution(participant, completion):
+    # 단 한 번만 전체 개수를 셉니다! 속도 O(N)
+    p_counter = Counter(participant)
+    c_counter = Counter(completion)
+    
+    # 개수가 다른 사람을 찾습니다.
+    for name in p_counter:
+        if p_counter[name] != c_counter[name]:
+            return name
+"""
