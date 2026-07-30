8ef0c3ce0921f6f9a7c5a90495c518bf2a77a71d
kiteha03 <kiteha03@gmail.com>
2026-07-30 14:50:36 +0900

    슬슬 문법의 한계가 온다

--- level1/07.30 성격 유형 검사하기/hayeon.py
+++ level1/07.30 성격 유형 검사하기/hayeon.py
@@ -1,1 +1,33 @@
-
+def solution(survey, choices):
+    answer = ''
+    
+    score = {
+        "R" : 0,
+        "T" : 0,
+        "C" : 0,
+        "F" : 0,
+        "J" : 0,
+        "M" : 0,
+        "A" : 0,
+        "N" : 0
+    }
+    
+    for i in range(len(choices)):
+        if choices[i] == 4:
+            continue
+            
+        elif choices[i] < 4:
+            score[survey[i][0]] += 4 - choices[i]
+        
+        elif choices[i] > 4:
+            score[survey[i][1]] += choices[i] - 4
+                
+    keys_list =list(score.keys())
+    
+    for i in range(0,8,2):
+        if score[keys_list[i]] >= score[keys_list[i+1]]:
+            answer += keys_list[i]
+        else:
+            answer += keys_list[i+1]
+            
+    return answer
