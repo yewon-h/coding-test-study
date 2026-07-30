a340b6c5a6a9de680a5c933ce0fbce6259daf536
yewon-h <yewon99111@gmail.com>
2026-07-30 14:01:19 +0900

    급해서 주석따윈 달지않ㅇ므

--- level1/07.30 성격 유형 검사하기/hyw.py
+++ level1/07.30 성격 유형 검사하기/hyw.py
@@ -1,1 +1,25 @@
-
+def solution(survey, choices):
+    answer = ''
+    personality = {'R': 0, 'T': 0, 'C': 0, 'F': 0, 'J': 0, 'M': 0, 'A': 0, 'N': 0}
+    
+    for i, j in zip(survey, choices):
+        
+        if j < 4:
+            personality[i[0]] += 4 - j
+            
+        if j > 4:
+            personality[i[1]] += j - 4
+            
+    i = 0
+    keys_list = list(personality.keys())
+    
+    while i < 8:
+        if personality[keys_list[i]] >= personality[keys_list[i+1]]:
+            answer += keys_list[i]
+            
+        else:
+            answer += keys_list[i+1]
+            
+        i += 2
+    
+    return answer
